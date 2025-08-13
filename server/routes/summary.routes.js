3d655d5bf31e875e9b3d2dd9dbb9a18d5914709a
Posaram Dewasi <157194720+posurabari@users.noreply.github.com>
2025-08-13 11:05:27 +0530

    Delete server directory

--- server/routes/summary.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import axios from "axios";
-import wrapAsync from "../utils/wrapAsync.js";
-import jwtAuth from "../middleware/jwtAuth";
-import { generateSummary } from "../controllers/summaryController.js";
-const router = express.Router();
-
-router.get('/', jwtAuth, wrapAsync(generateSummary));
--- server/utils/wrapAsync.js
+++
@@ -1,8 +0,0 @@
-export default (fn) => {
-    return (req, res, next) => {
-        fn(req, res, next)
-        .catch((err) => {
-            throw new Error(err);
-        });
-    };
-}
